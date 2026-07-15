f09979eb3cf4434769e219040d4360f678ec2846
vookotieekshitmohan <vookotieekshitmohan@gmail.com>
2026-07-15 21:56:04 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -120,3 +120,16 @@ function revealOnScroll(){
     cards.forEach(card=>observer.observe(card));
 
 }
+
+const cursor = document.querySelector(".cursor");
+const blur = document.querySelector(".cursor-blur");
+
+document.addEventListener("mousemove", (e) => {
+
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+
+    blur.style.left = e.clientX + "px";
+    blur.style.top = e.clientY + "px";
+
+});
